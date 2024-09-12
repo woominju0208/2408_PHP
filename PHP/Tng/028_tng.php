705cbd79910562ef530f33ee9fb28b9a0a284889
woominju0208 <alswn5033@gmail.com>
2024-09-12 10:25:42 +0900

    240912 PHP

--- PHP/Tng/028_tng.php
+++ PHP/Tng/028_tng.php
@@ -19,6 +19,21 @@ for($i = 1; $i <= 100; $i++) {
     echo $i."\n"; 
 }
 
+// continue; 가 아닌 else 로 쓰기
+for($i = 1; $i <= 100; $i++) {
+    if(($i%3) === 0) {
+        echo "짝\n";
+    } else {
+        echo $i;
+    }        
+    if($i !== 100) {
+        echo ",";
+    }
+}
+
+
+// continue를 적지 않으면 3의배수에서 짝이 나오고 밑에 echo $i."\n";로 인해 짝 과 3의배수 값까지 같이 나온다.
+// continue : 처리중에 continue가 나오면 그이후의 처리는 건너뛰고 다음 루프 진행
 
 // 2. 반복문을 이용하여 급여가 5000이상이고, 성별이 남자인 사원의 id와 이름을 출력해주세요.
 //  출력 예)
@@ -37,12 +52,12 @@ $arr = [
 //         echo $item["id"],$item["name"]."\n";
 //     }
 // }
-
 // $item["gender"] === "M"
 
 
 foreach($arr as $item) {
-    if(($item["salary"] >= "5000") && ($item["gender"] === "M")) {
+    if(((int)$item["salary"] >= "5000") && ($item["gender"] === "M")) {
         echo "id:".$item["id"]," name:".$item["name"]."\n";
     }
 }
+// (int)를 넣음으로써 데이터타입을 명확하게 인식
