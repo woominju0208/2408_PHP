0eb1b3ee8e97df0db33da42c535189f16dd7664c
woominju0208 <alswn5033@gmail.com>
2024-10-30 23:46:44 +0900

    241030 javascript 연습

--- JavaScript/js/test.js
+++ JavaScript/js/test.js
@@ -1,15 +1,27 @@
-const TEST = document.querySelectorAll('.none-li');
-// TEST.style.color = 'blue';
+// const TEST = document.querySelectorAll('.none-li');
+// // TEST.style.color = 'blue';
 
-TEST.forEach((element, idx) => {
-    element.style.color = 'blue'
-});
+// TEST.forEach((element, idx) => {
+//     element.style.color = 'blue'
+// });
 
 const NEW_LI = document.createElement('li');
 NEW_LI.textContent = '장기';
-NEW_LI.style.color = 'red';
 
 const GAME = document.querySelector('#ul');
 
 const TARGET = document.querySelector('#ul :nth-child(4)');
 GAME.insertBefore(NEW_LI, TARGET);
+
+const BGCOLOR = document.querySelector('#ul :nth-child(10)');
+BGCOLOR.style.background = 'beige';
+
+const LI = document.querySelectorAll('li');
+LI.forEach((Element, idx)  => {
+    if(idx % 2 === 1) {
+        Element.style.color = 'blue';
+    } else{
+        Element.style.color = 'red';
+    }
+}); 
+
