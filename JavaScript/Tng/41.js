859bbe492fea08b5740e5f8995c82ca2343ea77d
woominju0208 <alswn5033@gmail.com>
2024-11-01 11:41:42 +0900

    241101 javascript

--- JavaScript/Tng/41.js
+++ JavaScript/Tng/41.js
@@ -1,92 +1,52 @@
-// 1.실시간 시계만들기
-// 멈춰 클릭시 시간멈춤
-// 재시작 클릭시 현재시간으로 리셋
 
-// html id불러오기
-const TARGET = document.querySelector('#clock');
-// 시간 id 배열 만들어주기
-let id = [];
-// 시계함수 불러오기
-clock();
-// 시계 실행
-let realClock = setInterval(clock,1000);
-// 시간 id  realClock에 넣어주기
-id.push(realClock);
+// lpad 앞에 0 함수화
+function leftPadZero(target, length) {
+    return String(target).padStart(length, '0')
+}
 
-// 시계 만들기
-function clock() {
+// 시계 함수화
+function getDate() {
+    // 새로운 객체 date를 인스턴스화
     const NOW = new Date();
+    let hour = NOW.getHours();  // 시간획득(24시 포멧)
+    let minute = NOW.getMinutes();  // 분 획득
+    let second = NOW.getSeconds();  // 초 획득
+    let ampm = hour < 12 ? '오전' : '오후'; // 오전, 오후
+    let hour12 = hour < 13 ? hour : hour - 12;  // 시간(12시 포멧)
     
-    // 두글자해서 앞에 0 붙이기 
+    let timeFormat = 
+        `${ampm} ${leftPadZero(hour12, 2)}:${leftPadZero(minute, 2)}:${leftPadZero(second, 2)}`;
 
-    // 시분초 구하기
-    const HOUR = addLpadZeroHour(NOW.getHours(), 5);
-    const MINUTE = addLpadZero(NOW.getMinutes(), 2);
-    const SECOND = addLpadZero(NOW.getSeconds(), 2);
+    document.querySelector('#time').textContent = timeFormat;
+    // body 안에 id가 time인 부분에 timeFormat을 text로 출력
+}
 
-    function addLpadZeroHour (num, length) {
-        if(num === 12) {
-            return String().padStart(length, '오후 12');
-        } else if(num === 24) {
-            return String().padStart(length, '오전 00');
-        } else if(num < 12) {
-            return String(num).padStart(length, '오전 0');
-        } else if(num > 12) {
-            return String(num - 12).padStart(length, '오후 0');
+
+// 즉시실행함수를 사용(모듈화)
+// 다른 부분에선 사용할수없고 필요한 멈춰,재시작에서만 실행할수 있도록 즉시실행함수에 넣음(메모리 감소)
+(() => {
+    getDate();  // 최초의 시계 바로 실행
+    // 새롭게 시간 값을 받아오기 때문에 const시에는 재할당이 불가 해 let으로 받아옴 let은 값이 계속 바뀔수 있다
+    // 전역으로 처리해서 재시작,멈춰일때 맞는 id를 가져올수 있게 함 
+    // 타이머 시간은 500 이 적당 1000은 한박자 느리고 100은 너무 많이 요청을 보냄
+    let intervalId = null;  // intervalId가 뭐가 들어가는지 보여주기 위해 null 작성
+    intervalId = setInterval(getDate, 500);
+    
+    // 멈춰 버튼
+    document.querySelector('#btn-stop').addEventListener('click', () => {
+        clearInterval(intervalId);
+        intervalId = null;
+    });
+    
+    // 재시작 버튼
+    // 재시작 누른후면 intervalId 가 다르기 때문에 밖에 전역으로 만든 let intervalId 로 두고
+    // 재시작시에도 intervalId = setInterval(getDate, 500);으로 처리 함으로써 전역 let intervalId에 담기게 된다.
+    // 그 후 멈춰를 누르면 전역 let intervalId 에 저장된 id를 지우게 된다.
+    // intervalId가 비었을시에만 id를 넣어라
+    document.querySelector('#btn-restart').addEventListener('click', () => {
+        getDate();  // 재시작에도 최초의 시계 바로 실행
+        if(intervalId === null) {
+            intervalId = setInterval(getDate, 500);
         }
-    }   
-    function addLpadZero (num, length) {
-            return String(num).padStart(length, '0');
-        }
-
-    // const AMPM = 'AM';
-    // if(HOUR > 12) {
-    //     const AMPM = 'PM';
-    //     HOUR %= 12;
-    // }
-
-    // 시분초 바꿈
-    // const FORMAT_DATE = `${HOUR}:${MINUTE}:${SECOND}`;
-    // console.log(FORMAT_DATE);
-    TARGET.textContent = `${HOUR}:${MINUTE}:${SECOND}`;
-    const BODY = document.querySelector('p');
-    BODY.appendChild(TARGET);
-
-    // TARGET_APM.innerHTML = `${AMPM}`;
-}
-// clock 함수 끝
-
-const STOP = document.querySelector('#stop');
-const RESTART = document.querySelector('#restart');
-
-// 멈춰버튼 함수
-function stopBtn() {
-    // 시간 id를 배열안에 넣고 시계 멈추기
-    id.forEach(realClock => {
-        clearTimeout(realClock);
-    })
-    // 멈추면 시간 id 를 [] 빈배열로 다 삭제하기
-    id = [];      
-    // if(realClock === null) {
-    //     clock();
-    //     realClock = setInterval(clock,1000);
-    // }
-}
-
-// 재시작버튼 함수
-function restartBtn() {
-    clock();
-    realClock = setInterval(clock,1000);
-    // 재시작 누를시 시간id 넣어주기
-    id.push(realClock);
-    // if(realClock === null) {
-    //     clock();
-    //     realClock = setInterval(clock,1000);
-    // } 
-}
-// 재시작누르고 멈춰를 누르면 실행이 안되네...
-
-
-
-STOP.addEventListener('click', stopBtn);
-RESTART.addEventListener('click', restartBtn);
+    });
+})();
