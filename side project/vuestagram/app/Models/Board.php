98f589ed99c995220f4539263e931a004f575965
woominju0208 <alswn5033@gmail.com>
2024-11-25 18:13:38 +0900

    241125 ~모델 Board 수정

--- side project/vuestagram/app/Models/Board.php
+++ side project/vuestagram/app/Models/Board.php
@@ -27,6 +27,6 @@ class Board extends Model
 
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
-        // 다중인곳은 belongsTO
+        // 다중인곳은 belongsTo
     }
 }
